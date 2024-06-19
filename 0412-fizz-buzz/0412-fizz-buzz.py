f4327e14da05a04206de24f41a179a50d7d32f79
Fedya <77743898+fefumo@users.noreply.github.com>
2024-06-19 05:07:39 +0300

    Time: 41 ms (6.09%), Space: 12.3 MB (93.88%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type i: int
+        :rtype: List[str]
+        """
+        result = []
+        s = ""
+        for i in range (1, n+1):
+            if i % 3 == 0 and i % 5 == 0:
+                s = "FizzBuzz"
+            elif i % 3 == 0:
+                s = "Fizz"
+            elif i % 5 == 0:
+                s = "Buzz"
+            else:
+                s = str(i)
+            result.append(s)
+                        
+        return result

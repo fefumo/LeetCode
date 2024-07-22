359ccf5ac68ff8306489bf6eb1db3e63561fdc56
Fedya <77743898+fefumo@users.noreply.github.com>
2024-07-22 15:31:19 +0300

    Time: 33 ms (40.77%), Space: 26 MB (69.75%) - LeetHub

--- 2418-sort-the-people/2418-sort-the-people.cpp
+++ 2418-sort-the-people/2418-sort-the-people.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
-        int i = 0;
-        bool swapped;
-        int n = heights.size();
-        do{
-            swapped = false;
-            for (int i = 0; i < n - 1; i++){
-                if (heights[i] < heights[i+1]){
-                    iter_swap(heights.begin() + i, heights.begin() + (i + 1));
-                    iter_swap(names.begin() + i, names.begin() + (i + 1));
-                    swapped = true;
-                }
-            }
-            --n;
-        }while (swapped);
+        vector <pair<int, string>> heightNamePairs;
+        
+        for (int i = 0; i < heights.size(); i++){
+            heightNamePairs.emplace_back(heights[i], names[i]);
+        }
+        
+        sort(heightNamePairs.begin(), heightNamePairs.end(), greater<pair<int, string>>());
+        
+        for (int i = 0; i < names.size(); i++){
+            names[i] = heightNamePairs[i].second;
+        }
 
         return names;
     }

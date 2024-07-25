0e1fe5412e1654172208bfb61794df644d309485
Fedya <77743898+fefumo@users.noreply.github.com>
2024-07-25 14:23:14 +0300

    Time: 643 ms (18.66%), Space: 267 MB (21.71%) - LeetHub

---
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        mergeSort(nums, 0, nums.size() - 1);
+        return nums;
+    }
+    void merge(vector<int>& arr, int left, int mid, int right){
+        int subarr1 = mid - left + 1;
+        int subarr2 = right - mid;
+        vector<int>* leftarr = new vector<int>(subarr1);
+        vector<int>* rightarr = new vector<int>(subarr2);
+
+        for(int i = 0; i < subarr1; i++ ){
+            (*leftarr)[i] = arr[left + i];
+        }
+        for (int j = 0; j < subarr2; j++){
+            (*rightarr)[j] = arr[mid + 1 + j];
+        }
+        int index_of_subarr1 = 0, index_of_subarr2 = 0;
+        int index_of_merged_arr = left;
+
+        while (index_of_subarr1 < subarr1 && index_of_subarr2 < subarr2){
+            if ((*leftarr)[index_of_subarr1] <= (*rightarr)[index_of_subarr2]){
+                arr[index_of_merged_arr] = (*leftarr)[index_of_subarr1];
+                index_of_subarr1++;
+            }
+            else{
+                arr[index_of_merged_arr] = (*rightarr)[index_of_subarr2];
+                index_of_subarr2++;
+            }
+            index_of_merged_arr++;
+        }
+
+        //if anything is left
+        while(index_of_subarr1 < subarr1){
+            arr[index_of_merged_arr] = (*leftarr)[index_of_subarr1];
+            index_of_merged_arr++;
+            index_of_subarr1++;
+        }
+
+        while(index_of_subarr2 < subarr2){
+            arr[index_of_merged_arr] = (*rightarr)[index_of_subarr2];
+            index_of_merged_arr++;
+            index_of_subarr2++;
+        }
+
+        delete leftarr;
+        delete rightarr;
+    }
+
+    void mergeSort(vector<int>& arr, int begin, int end){
+        if (begin >= end){
+            return;
+        }
+        int mid = begin + (end - begin) / 2;
+        mergeSort(arr, begin, mid);
+        mergeSort(arr, mid + 1, end);
+        merge(arr, begin, mid, end);
+    }
+};

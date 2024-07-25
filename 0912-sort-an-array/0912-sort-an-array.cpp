67639e4299c543b60d63de71be39a33fcca0f6e6
Fedya <77743898+fefumo@users.noreply.github.com>
2024-07-25 14:26:08 +0300

    Time: 448 ms (30.07%), Space: 178.4 MB (28.80%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -7,25 +7,25 @@ public:
     void merge(vector<int>& arr, int left, int mid, int right){
         int subarr1 = mid - left + 1;
         int subarr2 = right - mid;
-        vector<int>* leftarr = new vector<int>(subarr1);
-        vector<int>* rightarr = new vector<int>(subarr2);
+        vector<int> leftarr (subarr1);
+        vector<int> rightarr (subarr2);
 
         for(int i = 0; i < subarr1; i++ ){
-            (*leftarr)[i] = arr[left + i];
+            (leftarr)[i] = arr[left + i];
         }
         for (int j = 0; j < subarr2; j++){
-            (*rightarr)[j] = arr[mid + 1 + j];
+            (rightarr)[j] = arr[mid + 1 + j];
         }
         int index_of_subarr1 = 0, index_of_subarr2 = 0;
         int index_of_merged_arr = left;
 
         while (index_of_subarr1 < subarr1 && index_of_subarr2 < subarr2){
-            if ((*leftarr)[index_of_subarr1] <= (*rightarr)[index_of_subarr2]){
-                arr[index_of_merged_arr] = (*leftarr)[index_of_subarr1];
+            if ((leftarr)[index_of_subarr1] <= (rightarr)[index_of_subarr2]){
+                arr[index_of_merged_arr] = (leftarr)[index_of_subarr1];
                 index_of_subarr1++;
             }
             else{
-                arr[index_of_merged_arr] = (*rightarr)[index_of_subarr2];
+                arr[index_of_merged_arr] = (rightarr)[index_of_subarr2];
                 index_of_subarr2++;
             }
             index_of_merged_arr++;
@@ -33,19 +33,19 @@ public:
 
         //if anything is left
         while(index_of_subarr1 < subarr1){
-            arr[index_of_merged_arr] = (*leftarr)[index_of_subarr1];
+            arr[index_of_merged_arr] = (leftarr)[index_of_subarr1];
             index_of_merged_arr++;
             index_of_subarr1++;
         }
 
         while(index_of_subarr2 < subarr2){
-            arr[index_of_merged_arr] = (*rightarr)[index_of_subarr2];
+            arr[index_of_merged_arr] = (rightarr)[index_of_subarr2];
             index_of_merged_arr++;
             index_of_subarr2++;
         }
 
-        delete leftarr;
-        delete rightarr;
+        //delete leftarr;
+        //delete rightarr;
     }
 
     void mergeSort(vector<int>& arr, int begin, int end){
